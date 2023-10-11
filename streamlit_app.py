4663fbdac0c497f2e744d2a2fe749e8fa9642f73
thejitae <thejitae@gmail.com>
2023-10-11 10:33:52 +0900

    add fruit adder

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,9 +55,9 @@ if streamlit.button('Get Fruit Load List'):
     streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
-def insert_row_snowflake(new_fruit):
+def insert_row_snowflake(add_my_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        my_cur.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
         return "Thanks for adding " + add_my_fruit + "!"
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
